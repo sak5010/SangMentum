8f088affbb61378ba6489a1746ef166f3a3fb913
sanghyeonKim <sak5010@naver.com>
2022-05-24 00:05:48 +0900

    news 로테이션 기능 추가(미완)
    여기에 API 적용해야함

---
+++ js/news.js
@@ -0,0 +1,79 @@
+let tmpNews = [
+  "news1",
+  "news2",
+  "news3",
+  "news4",
+  "news5",
+  "news6",
+  "news7",
+  "news8",
+  "news9",
+  "news10",
+];
+
+const timer = 2000;
+
+const first = document.querySelector("#news-first");
+const second = document.querySelector("#news-second");
+const third = document.querySelector("#news-third");
+
+let move = 2;
+let dataCnt = 1;
+let listCnt = 1;
+
+first.children[0].innerHTML = tmpNews[0];
+
+setInterval(() => {
+  if (move === 2) {
+    first.classList.remove("card_sliding");
+    first.classList.add("card_sliding_after");
+
+    second.classList.remove("card_sliding_after");
+    second.classList.add("card_sliding");
+
+    third.classList.remove("card_sliding_after");
+    third.classList.remove("card_sliding");
+
+    move = 0;
+  } else if (move === 1) {
+    first.classList.remove("card_sliding_after");
+    first.classList.add("card_sliding");
+
+    second.classList.remove("card_sliding_after");
+    second.classList.remove("card_sliding");
+
+    third.classList.remove("card_sliding");
+    third.classList.add("card_sliding_after");
+
+    move = 2;
+  } else if (move == 0) {
+    first.classList.remove("card_sliding_after");
+    first.classList.remove("card_sliding");
+
+    second.classList.remove("card_sliding");
+    second.classList.add("card_sliding_after");
+
+    third.classList.remove("card_sliding_after");
+    third.classList.add("card_sliding");
+
+    move = 1;
+  }
+
+  if (dataCnt < tmpNews.length - 1) {
+    document.querySelector(".news-box").children[
+      listCnt
+    ].children[0].innerHTML = tmpNews[dataCnt];
+    dataCnt++;
+  } else if (dataCnt == tmpNews.length - 1) {
+    document.querySelector(".news-box").children[
+      listCnt
+    ].children[0].innerHTML = tmpNews[dataCnt];
+    dataCnt = 0;
+  }
+
+  if (listCnt < 2) {
+    listCnt++;
+  } else if (listCnt == 2) {
+    listCnt = 0;
+  }
+}, timer);
