e5aed1866896889fb946e8e972c1e514355e76ec
sanghyeonKim <sak5010@naver.com>
2022-05-30 18:39:47 +0900

    스크롤텍스트 수정

--- public/js/news.js
+++ public/js/news.js
@@ -20,65 +20,6 @@ const third = document.querySelector("#news-third");
 const searchForm = document.querySelector("#search-form");
 const searchText = document.querySelector("#search-text");
 
-let move = 2;
-let dataCnt = 1;
-let listCnt = 1;
-
-setInterval(() => {
-  if (move === 2) {
-    first.classList.remove("card_sliding");
-    first.classList.add("card_sliding_after");
-
-    second.classList.remove("card_sliding_after");
-    second.classList.add("card_sliding");
-
-    third.classList.remove("card_sliding_after");
-    third.classList.remove("card_sliding");
-
-    move = 0;
-  } else if (move === 1) {
-    first.classList.remove("card_sliding_after");
-    first.classList.add("card_sliding");
-
-    second.classList.remove("card_sliding_after");
-    second.classList.remove("card_sliding");
-
-    third.classList.remove("card_sliding");
-    third.classList.add("card_sliding_after");
-
-    move = 2;
-  } else if (move == 0) {
-    first.classList.remove("card_sliding_after");
-    first.classList.remove("card_sliding");
-
-    second.classList.remove("card_sliding");
-    second.classList.add("card_sliding_after");
-
-    third.classList.remove("card_sliding_after");
-    third.classList.add("card_sliding");
-
-    move = 1;
-  }
-
-  if (dataCnt < tmpNews.length - 1) {
-    document.querySelector(".news-box").children[
-      listCnt
-    ].children[0].innerHTML = tmpNews[dataCnt];
-    dataCnt++;
-  } else if (dataCnt == tmpNews.length - 1) {
-    document.querySelector(".news-box").children[
-      listCnt
-    ].children[0].innerHTML = tmpNews[dataCnt];
-    dataCnt = 0;
-  }
-
-  if (listCnt < 2) {
-    listCnt++;
-  } else if (listCnt == 2) {
-    listCnt = 0;
-  }
-}, timer);
-
 function handleSearchSubmit(event) {
   event.preventDefault();
   let word = searchText.value;
